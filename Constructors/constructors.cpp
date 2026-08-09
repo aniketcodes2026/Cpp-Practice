233d343cf42677dfbe27b2682b52bc06157087db
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-08-09 21:24:54 +0530

    Refactor constructor characteristics comments
    Updated comments to use block comment syntax.

--- Constructors/constructors.cpp
+++ Constructors/constructors.cpp
@@ -35,10 +35,10 @@ int main()
     return 0;
 }
 
-//CHARACTERISTICS OF CONSTRUCTORS
+/*CHARACTERISTICS OF CONSTRUCTORS
 
-//1.It should be declared in the public section of the class.
-//2.They are automatically invoked whenever the object is created.
-//3.They cannot return values and do not have return type.
-//4.It can have default arguments.
-//5.We cannot refer to their address.
+1.It should be declared in the public section of the class.
+2.They are automatically invoked whenever the object is created.
+3.They cannot return values and do not have return type.
+4.It can have default arguments.
+5.We cannot refer to their address.*/
